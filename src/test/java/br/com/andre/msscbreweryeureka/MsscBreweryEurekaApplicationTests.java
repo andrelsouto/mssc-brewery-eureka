e0cc69f1e842fc9224e7b81763af02f125f9fce6
André Luiz <andre.andrade@vsoft.com.br>
2021-01-26 20:27:52 -0300

    Initial Add

---
+++ src/test/java/br/com/andre/msscbreweryeureka/MsscBreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andre.msscbreweryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
